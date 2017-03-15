8d3f685bb69e7a0d10dada4c61d4be03a3e3c731
jorge-gs <jorgeluis.garcia@ujcv.edu.hn>
2017-03-14 23:21:59 -0600

    Added sorting menu
    Added sorting menu options
    Set up splash screen

--- app/src/main/java/com/example/universidad/pokedex/MainActivity.java
+++ app/src/main/java/com/example/universidad/pokedex/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.universidad.pokedex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.sort_region:
+                Toast.makeText(getBaseContext(), "Regional Pokédex", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sort_generation:
+                Toast.makeText(getBaseContext(), "Pokémon by generation", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.sort_national:
+                Toast.makeText(getBaseContext(), "National Pokédex", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/universidad/pokedex/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.example.universidad.pokedex;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        this.startActivity(intent);
+    }
+}
